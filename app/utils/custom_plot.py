a7d6b5459ad91c1d801a393ff85ca0ffbf005e31
o.filali <o.filali@studenti.unica.it>
2022-04-02 22:14:58 +0200

    Implementation of API for plotted results
    routes/get_plot serves a PNG of the plot image generated with
    the data provided to utils/custom_plot

---
+++ app/utils/custom_plot.py
@@ -0,0 +1,24 @@
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+def generate(labels, values):
+
+    colors = ["olivedrab", "yellowgreen", "seagreen", "teal", "palegreen"]
+
+    fig = Figure(figsize=(13,6), dpi=150)
+
+    axis = fig.add_subplot(1, 1, 1)
+    axis.barh(labels, values, color=colors)
+
+    axis.set_title("CLASSIFICATION RESULTS")
+    axis.set_xlabel("SCORE %")
+    axis.set_ylabel("LABEL")
+    axis.grid(True)
+    axis.invert_yaxis()
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+
+    return output.getvalue()
